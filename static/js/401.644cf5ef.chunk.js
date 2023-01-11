eaba0c6daf89d55a87067eaef6a67e69cf2f58cf
maxexc <104863585+maxexc@users.noreply.github.com>
2023-01-11 07:04:15 +0000

    Deploying to gh-pages from @ maxexc/goit-react-hw-05-movies@26081f41c7d489cb8a2ce03352f843fff26bd0be 🚀

--- static/js/401.644cf5ef.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[401],{1401:function(e,n,r){r.r(n),r.d(n,{default:function(){return s}});var i,t=r(168),o=r(7691).ZP.div(i||(i=(0,t.Z)(["\n    display: flex;\n    align-items: center;\n    flex-direction: column;\n    font-size: 32px;  \n"]))),c=r(184),s=function(){return(0,c.jsxs)(o,{children:[(0,c.jsx)("h1",{style:{color:"orangered"},children:"Error 404"}),(0,c.jsx)("p",{children:"Page not found"})]})}}}]);
-//# sourceMappingURL=401.644cf5ef.chunk.js.map
